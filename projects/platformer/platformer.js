3ffd4084678e099710203c12e106a77ca4a7ad45
NV-KAS <nate.g.vail@gmail.com>
2024-09-13 16:59:19 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -35,19 +35,25 @@ $(function () {
     /////////////////////////////////////////////////
 
     // TODO 1
-    createPlatform(1350,650,100,20)
-    createPlatform(1200,550,27,50)
-    createPlatform(1100,320,20,50)
-    createPlatform(1350,450,100,20)
-    createPlatform(1250,320,27,50)
-    createPlatform(180,300,700,20)
+    createPlatform(1350,650,1,20)
+    createPlatform(1200,550,1,50)
+    createPlatform(1100,320,2,50)
+    createPlatform(1350,450,1,20)
+    createPlatform(1250,320,2,50)
+    createPlatform(700,300,1,20)
+    createPlatform(800,250,1,50)
+    createPlatform(700,135,1,175)
+    createPlatform(800,300,100,1)
+    createPlatform(300,300,250,20)
+    createPlatform(100,0,10,500)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("database",350,175,1,0)
+    createCollectable("database",1300,0,0.25,1)
+    createCollectable("database",700,0,1,0)
 
     
     // TODO 3
@@ -55,9 +61,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+    createCannon("right", 100, 6500, 500, 200)
+    createCannon("top", 1350, 3000, 5, 200)
+    createCannon("bottom", 1325, 1, 10, 200000000)
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
